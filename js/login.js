b62a5a46645bbe395967ccfcd97a81f8034cf51c
Eric Snover <ersnover@gmail.com>
2019-07-30 12:29:03 -0500

    final tweaks to login/reg/user-auth

--- js/login.js
+++ js/login.js
@@ -2,19 +2,31 @@ const loginButton = document.getElementById('loginButton')
 const loginEmailField = document.getElementById('loginEmail')
 const loginPasswordField = document.getElementById('loginPassword')
 
+loginPasswordField.addEventListener('keyup', e => {
+    if (e.keyCode === 13) {
+        loginButton.click()
+    }
+})
+
 loginButton.addEventListener('click', () => {
-    email = loginEmailField.value
-    password = loginPasswordField.value
+    let email = loginEmailField.value
+    let password = loginPasswordField.value
 
     loginEmailField.value = ""
     loginPasswordField.value = ""
 
     firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(window.open("index.html"))
     .catch(error => {
         var errorCode = error.code;
         var errorMessage = error.message;
         alert(`Error ${errorCode}\n\n${errorMessage}`)
     });
-    
+})
+
+// redirect on login
+firebase.auth().onAuthStateChanged(user => {
+
+    if (user) {     //if a user is logged in
+        window.location = "index.html"  //redirects to index.html
+    }
 })
