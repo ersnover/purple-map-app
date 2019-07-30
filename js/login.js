d3d9f45e96e4801e7d7522040ccecdf25ca4fcc7
Eric Snover <ersnover@gmail.com>
2019-07-30 11:42:31 -0500

    minor changes before master pull

--- js/login.js
+++ js/login.js
@@ -10,10 +10,11 @@ loginButton.addEventListener('click', () => {
     loginPasswordField.value = ""
 
     firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(window.open("index.html"))
     .catch(error => {
         var errorCode = error.code;
         var errorMessage = error.message;
         alert(`Error ${errorCode}\n\n${errorMessage}`)
     });
-    window.open("index.html")
+    
 })
