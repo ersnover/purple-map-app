bfd3bd2913752858a7cc29bc8feb0989c34ef03e
Eric Snover <ersnover@gmail.com>
2019-08-01 15:29:11 -0500

    finished integration up to saving current search in firestore

---
+++ js/addressAndCriteria.js
@@ -0,0 +1,101 @@
+// GET ADDRESS
+let addressInput = document.getElementById("addressInput")
+let addressIntakeBtn = document.getElementById("addressIntakeBtn")
+let addressRegEx = /^\d+\s[A-z]+\s[A-z]+/
+let text = document.getElementById("text")
+
+let autocomplete = new google.maps.places.Autocomplete(addressInput)
+autocomplete.setFields(['address_components', 'place_id', 'name'])
+
+addressIntakeBtn.addEventListener("click", validateAddress)
+addressInput.addEventListener("keypress", event=>{
+    if(event.keyCode == 13){
+        validateAddress()
+    }
+})
+
+function validateAddress(){
+    let address = addressInput.value
+    if(addressRegEx.test(address) == false){
+        text.innerHTML = "Enter a valid address."
+    }else{
+        text.innerHTML =''
+        replaceDiv()
+        finalAddress = address
+    }
+}
+
+// REPLACE DIV
+let addressDiv = document.getElementById("addressIntakeDiv")
+let addressDivParent = addressDiv.parentNode
+let preferencesDiv = document.getElementById("search-criteria-div")
+
+function replaceDiv(){
+    preferencesDiv.style.display = "flex"
+    addressDivParent.replaceChild(preferencesDiv, addressDiv)
+}
+
+// POPULATE SEARCH CRITERIA FROM CRITERIA STATS OBJECT
+const searchCriteriaDiv = document.getElementById('search-criteria-div')
+
+placeTypes = Object.keys(criteriaStats)
+
+placeTypes.map((type, index) => {
+    
+    const googleId = criteriaStats[type].googleidname
+    const placeDisplayName =criteriaStats[type].placeDisplayName
+
+    const markup = `
+    
+    <label for="place-type-${index}" data-place = "${googleId}" id="${googleId}" class="place-type">${placeDisplayName}</label>
+    <input type="checkbox" name="place-type-${index}" id="place-type-${index}" class="place-type-checkbox"  data-selectid="select-${index}"> 
+    <select  id="select-${index}" class="importance-selector">
+        <option value="${highImp}">${highImp}</option>
+        <option value="${medImp}">${medImp}</option>
+        <option value="${lowImp}">${lowImp}</option>
+    </select> <br>
+    `
+
+    searchCriteriaDiv.insertAdjacentHTML('beforeend', markup)
+})
+
+// Show importance selector only if place type is checked
+let allPlaceTypeCheckboxes = document.querySelectorAll('.place-type-checkbox')
+
+allPlaceTypeCheckboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', function () {
+        showPlaceTypeImportanceSelector(this)
+    })
+})
+
+function showPlaceTypeImportanceSelector(checkbox) {
+    let selectId = checkbox.dataset.selectid
+    let select = document.getElementById(selectId)
+    
+    if (checkbox.checked) {
+        select.style.display = 'inline-block'
+    } else {
+        select.style.display = 'none'
+    }
+}
+
+// SUBMIT CRITERIA AND GENERATE CRITERIA INPUT OBJECT
+
+
+function getCriteriaObjs() {
+
+    const criteriaInputObjs = []
+
+    allPlaceTypeCheckboxes.forEach(box => {
+        if(box.checked) {
+            let placeType = box.previousElementSibling.id
+            let selectId = box.dataset.selectid
+            let placeTypeImportance = document.getElementById(selectId).value
+
+            let obj = new CriteriaInputObj(placeType, placeTypeImportance)
+
+            criteriaInputObjs.push(obj)
+        }
+    })
+    return criteriaInputObjs        // array
+}
--- js/criteriaObjects.js
+++ js/criteriaObjects.js
@@ -1,7 +1,26 @@
+const highImp = "Very Important"
+const medImp = "Important"
+const lowImp = "Slightly Important"
+// edit importance displays here
+
+class CriteriaInputObj {
+    constructor(type, importance) {
+        this.type = type
+        this.importance = importance
+    }
+}
+class CriteriaOutputObj {
+    constructor(type, importance, placeIds) {
+        this.type = type
+        this.importance = importance
+        this.placeIds = placeIds
+    }
+}
+
 class ReportObject {                    // used in building of score report
-    constructor(address, parameterInfoArray, score, scale) {
+    constructor(address, criteriaInfoArray, score, scale) {
         this.address = address
-        this.parameterInfoArray = parameterInfoArray
+        this.criteriaInfoArray = criteriaInfoArray
         this.score = score
         this.scale = scale
     }
@@ -20,7 +39,7 @@ const criteriaStats = {     ///// can potentially add radius modifiers to diving
 
     restaurant: 
     {
-        gooogleidname: "restaurant",
+        googleidname: "restaurant",
         placeDisplayName: "Restaurants",
         avg: 13.16,
         max: 20
@@ -28,7 +47,7 @@ const criteriaStats = {     ///// can potentially add radius modifiers to diving
 
     park:
     {
-        gooogleidname: "park",
+        googleidname: "park",
         placeDisplayName: "Parks",
         avg: 3.88,
         max: 20
@@ -36,56 +55,56 @@ const criteriaStats = {     ///// can potentially add radius modifiers to diving
     
     bar:
     {
-        gooogleidname: "bar",
+        googleidname: "bar",
         placeDisplayName: "Bars",
         avg: 7.28,
         max:20
     },
     gym:
     {
-        gooogleidname: "gym",
+        googleidname: "gym",
         placeDisplayName: "Gyms",
         avg: 6.88,
         max: 20
     },
     atm:
     {
-        gooogleidname: "atm",
+        googleidname: "atm",
         placeDisplayName: "ATMs",
         avg: 9.64,
         max: 20
     },
     convenience_store:
     {
-        gooogleidname: "convenience_store",
+        googleidname: "convenience_store",
         placeDisplayName: "Convenience Stores",
         avg: 4.92,
         max: 20
     },
     bus_station:
     {
-        gooogleidname: "bus_station",
+        googleidname: "bus_station",
         placeDisplayName: "Bus Stations",
         avg: 13.4,
         max: 20
     },
     liquor_store:
     {
-        gooogleidname: "liquor_store",
+        googleidname: "liquor_store",
         placeDisplayName: "Liquor Stores",
         avg: 1.92,
         max: 15
     },
     post_office:
     {
-        gooogleidname: "post_office",
+        googleidname: "post_office",
         placeDisplayName: "Post Offices",
         avg: 0.4,
         max: 2
     },
     supermarket:
     {
-        gooogleidname: "supermarket",
+        googleidname: "supermarket",
         placeDisplayName: "Supermarkets",
         avg: 0.8,
         max: 5
