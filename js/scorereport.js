e4677ff585df51aacf2cf02b5c6e32dceb4d98f8
Kelsey Gaetjens <kjgaetjens@gmail.com>
2019-08-04 17:28:46 -0500

    score report styling - updated container sizing to accomodate variable number of parameter boxes

--- js/scorereport.js
+++ js/scorereport.js
@@ -1,6 +1,7 @@
 //load info before the rest of the things on the page happen
 let usersCollectionRef = db.collection('users')
 const usernameSpan = document.getElementById('userSpan')
+var algorithmObject
 
 firebase.auth().onAuthStateChanged(function(user) {
 
@@ -18,9 +19,10 @@ firebase.auth().onAuthStateChanged(function(user) {
         var userRef = usersCollectionRef.doc("Guest")   //if no user, checks "Guest" collection
     }
     
-    let algorithmObjectTest = userRef.collection('searches').doc('currentSearch')
-    algorithmObjectTest.get().then(function(obj) {
-        generatePage(obj.data())
+    let algorithmObjectDoc = userRef.collection('searches').doc('currentSearch')
+    algorithmObjectDoc.get().then(function(obj) {
+        algorithmObject = obj.data()
+        generatePage(algorithmObject)
     })
 })
 
@@ -35,7 +37,7 @@ function determineImportanceClass(parameterImportance) {
 }
 
 function determineScoreColor(score) {
-    if (score > 85) {
+    if (score >= 85) {
         return 'green'
     } else if (score > 50 && score < 85) {
         return 'yellow'
@@ -44,12 +46,11 @@ function determineScoreColor(score) {
     }
 }
 
-function generatePage(algorithmObject) {
+function generateMainScoreSection(algorithmObject) {
     let scoreAddress = algorithmObject['address']
     let appendedScoreAddress = scoreAddress.substring(0,scoreAddress.lastIndexOf(','))
     let score = algorithmObject['score']
     let scoreColor = determineScoreColor(score)
-    let parameterInfo = algorithmObject['criteriaArray']
 
     let boxSize = 80
     let radius = 15.91551
@@ -60,16 +61,6 @@ function generatePage(algorithmObject) {
     //score
     let addressHeader = document.getElementById('address-header')
     addressHeader.innerHTML = `${appendedScoreAddress}`
-    //change this so that it pull sthe score div and then creates the entire div interior and remove the score header thats already in my html file.
-    //set up color variable too
-    // let mainScoreContainer = document.getElementById('main-score-container')
-
-    // let boxSize = 40
-    // let radius = 15.91551
-    // let diameter = 31.83102
-    // let xVal = boxSize/2
-    // let yVal = boxSize-31.83102
-    // yVal = yVal/2
 
     let svgColor = document.getElementsByClassName("svg-color")[0]
     let scoreHeader = document.getElementsByClassName("score-header")[0]
@@ -81,16 +72,19 @@ function generatePage(algorithmObject) {
    
     
     scoreHeader.innerHTML=`${score}`
-    
- 
+}
 
-    //add variable for the importance value
-
+function generateDetailScoreSection(algorithmObject) {
+        //add variable for the importance value
 
     //parameter scores
+    let parameterInfo = algorithmObject['criteriaArray']
     let detailScoreContainer = document.getElementById('detail-score-container')
 
     let detailScoreArray =[]
+    let count = 1
+    let factorsHeader = document.getElementById('factors-header')
+    let factorsHeaderHeight = factorsHeader.clientHeight
     parameterInfo.forEach(function(parameterObj) { //change the type name
         let parameterType = parameterObj['type']
         let parameterImportance = parameterObj['importance']
@@ -99,57 +93,78 @@ function generatePage(algorithmObject) {
 
         let importanceClass = determineImportanceClass(parameterImportance)
         let formattedType = parameterType.replace('_',' ')
-        let detailScoreContainerHeight = detailScoreContainer.clientHeight
-        let divDimension = (detailScoreContainerHeight/2.25)
+        let divDimension = factorsHeaderHeight * 3.75
 
-        let div = `<div class="parameter-container ${importanceClass}" style="height:${divDimension}; width:${divDimension}" onclick="generateParameterDetailDiv('${parameterType}','${parameterImportance}','${parameterNumber}','${parameterScore}')">
+        let div = `<div class="parameter-container ${importanceClass}" id="parameter-container-${count}" style="height:${divDimension}; width:${divDimension}" onclick="generateParameterDetailDiv('${parameterType}','${parameterImportance}','${parameterNumber}','${parameterScore}')">
                         <h3 class="toTitleCase">${formattedType}</h3>
                         <h2>${parameterScore}</h2>
                     </div>`
 
         detailScoreArray.push(div)
+        count += 1
     })
     detailScoreContainer.innerHTML = detailScoreArray.join('')
+    let parameterContainer = document.getElementById('parameter-container-1')
+    let parameterContainerDimension = parameterContainer.clientHeight
+    let parameterContainerWidth = parameterContainerDimension + 40
+    let parameterContainerRowHeight = parameterContainerDimension + 40
+    let numParameters = detailScoreArray.length
+    let parameterContainersTotalWidth = parameterContainerWidth*numParameters
+    let secondScoreContainer = document.getElementById('second-score-container')
+    // let secondScoreContainerWidth = secondScoreContainer.clientWidth
+    let detailScoreContainerWidth = detailScoreContainer.clientWidth
 
-    //style things
-
-    //maybe have visual indicator of importance of each parameter
-    //maybe have credit score circle with red to green scale
-    //would have to add classes to divs accordingly and popualte image accordingly
-
+    //change this whole thing into a do while loop
+    if ( parameterContainersTotalWidth > detailScoreContainerWidth) {
+        let numParametersPerRow = (Math.floor(detailScoreContainerWidth/parameterContainerWidth))
+        let numRows = Math.ceil(numParameters/numParametersPerRow)
+        let parameterContainerTotalRowHeight = parameterContainerRowHeight * numRows
+        secondScoreContainer.style.height = factorsHeaderHeight + parameterContainerTotalRowHeight + 60
+    } else {
+        secondScoreContainer.style.height = factorsHeaderHeight + parameterContainerRowHeight + 60
+    }
 }
 
-    //parameter score details
-    function generateParameterDetailDiv(parameterType, parameterImportance, parameterNumber, parameterScore) {
-        let formattedType = parameterType.replace('_',' ')
-        let detailParameterModal = document.getElementById('detail-parameter-modal')
-        let detailParameterContainer = document.getElementById('detail-parameter-container')
-        let disclaimer = ''
-        let formattedParameterNumber = parameterNumber
-        if (parameterNumber == 20) {
-            disclaimer = `<p class="disclaimer">*The maximum number of nearby places per type used for our website is 20. There may be more than 20 ${formattedType}s within 1000 meters.</p>`
-            formattedParameterNumber += '*'
-        }
-        // detailParameterImportanceBar.className = determineImportanceClass(parameterImportance)
-        let content =  `<div id="detail-parameter-header">
-                            <span><h2 class="toTitleCase">${formattedType}</h2></span>
-                            <span><h1>${parameterScore}</h1></span>
-                        </div>
-                        <h4>Selected level of importance for ${formattedType}s:</h4>
-                        <h3>${parameterImportance}</h3>
-                        <h4>Number of ${formattedType}s within 1000 meters:</h4>
-                        <h3>${formattedParameterNumber}</h3>
-                        <h4>More information:</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        <img src="images/no-image-available.jpg">
-                        ${disclaimer}`
+function generatePage(algorithmObject) {
+    generateMainScoreSection(algorithmObject)
+    generateDetailScoreSection(algorithmObject)
+}
 
-        detailParameterContainer.innerHTML = content
-        detailParameterModal.style.display = 'block'
-    }
-
+//parameter score details
+function generateParameterDetailDiv(parameterType, parameterImportance, parameterNumber, parameterScore) {
+    let formattedType = parameterType.replace('_',' ')
     let detailParameterModal = document.getElementById('detail-parameter-modal')
-    let closeButton = document.getElementsByClassName('close')[0]
-    closeButton.onclick = function(){
-        detailParameterModal.style.display = "none";
+    let detailParameterContainer = document.getElementById('detail-parameter-container')
+    let disclaimer = ''
+    let formattedParameterNumber = parameterNumber
+    if (parameterNumber == 20) {
+        disclaimer = `<p class="disclaimer">*The maximum number of nearby places per type used for our website is 20. There may be more than 20 ${formattedType}s within 1000 meters.</p>`
+        formattedParameterNumber += '*'
     }
+    // detailParameterImportanceBar.className = determineImportanceClass(parameterImportance)
+    let content =  `<div id="detail-parameter-header">
+                        <span><h2 class="toTitleCase">${formattedType}</h2></span>
+                        <span><h1>${parameterScore}</h1></span>
+                    </div>
+                    <h4>Selected level of importance for ${formattedType}s:</h4>
+                    <h3>${parameterImportance}</h3>
+                    <h4>Number of ${formattedType}s within 1000 meters:</h4>
+                    <h3>${formattedParameterNumber}</h3>
+                    <h4>More information:</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <img src="images/no-image-available.jpg">
+                    ${disclaimer}`
+
+    detailParameterContainer.innerHTML = content
+    detailParameterModal.style.display = 'block'
+}
+
+let detailParameterModal = document.getElementById('detail-parameter-modal')
+let closeButton = document.getElementsByClassName('close')[0]
+closeButton.onclick = function(){
+    detailParameterModal.style.display = "none";
+}
+
+window.addEventListener('resize', function() {
+    generateDetailScoreSection(algorithmObject)
+    })
